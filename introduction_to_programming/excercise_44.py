143a0427ae9bc725fc3134ff5c73158ded7b07de
akashm-25 <akashaadith@gmail.com>
2020-03-28 15:05:45 +0530

    inital

---
+++ introduction_to_programming/excercise_44.py
@@ -0,0 +1,15 @@
+deno=int(input('Enter the denomination'))
+if deno==1:
+	print('Gorge washington')
+elif deno==2:
+	print('thomas jefferson')
+elif deno==5:
+	print('abraham lincon')
+elif deno==10:
+	print('alexander hamilton')
+elif deno==20:
+	print('andrew jackson')
+elif deno==50:
+	print('ulysses')
+elif deno==100:
+	print('benjamin franklin')
